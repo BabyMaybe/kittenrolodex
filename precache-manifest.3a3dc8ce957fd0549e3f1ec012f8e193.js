b9d95d86464a8394b2726620364025237ff97ad6
Eric Varela <eric_varela@mac.com>
2020-01-12 16:50:39 -0500

    Updates

---
+++ precache-manifest.3a3dc8ce957fd0549e3f1ec012f8e193.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ff930e7e5caa8fd7e272446435f8b56",
+    "url": "/kittenrolodex/index.html"
+  },
+  {
+    "revision": "5fd468f08516f6463a58",
+    "url": "/kittenrolodex/static/css/main.b7a69bbe.chunk.css"
+  },
+  {
+    "revision": "20eb7b3612173e07f0b7",
+    "url": "/kittenrolodex/static/js/2.4f0a8636.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/kittenrolodex/static/js/2.4f0a8636.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5fd468f08516f6463a58",
+    "url": "/kittenrolodex/static/js/main.173a029f.chunk.js"
+  },
+  {
+    "revision": "16d2f8fd1d2eb24ad7e1",
+    "url": "/kittenrolodex/static/js/runtime-main.0e6ad252.js"
+  }
+]);
